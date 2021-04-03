dbb0ae51f896798704bc11248a15883196ea08e4
aria <aryashahbazi@gmail.com>
2021-04-03 13:52:32 +0430

    Build

---
+++ Build/tests/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: G:/C++/AW/3D_distance/3D-distance/tests
+# Build directory: G:/C++/AW/3D_distance/3D-distance/Build/tests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(myLibrary.unit_tests "G:/C++/AW/3D_distance/3D-distance/Build/unit_tests.exe")
+set_tests_properties(myLibrary.unit_tests PROPERTIES  _BACKTRACE_TRIPLES "G:/C++/AW/3D_distance/3D-distance/tests/CMakeLists.txt;22;add_test;G:/C++/AW/3D_distance/3D-distance/tests/CMakeLists.txt;0;")
