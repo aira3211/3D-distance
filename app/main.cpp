d74c575aa095866d3523d4a99e48f926051f30f4
aria <aryashahbazi@gmail.com>
2021-04-03 13:50:43 +0430

    3D distance
    For chapter 3

--- app/main.cpp
+++ app/main.cpp
@@ -1,12 +1,35 @@
-
 #include <iostream>
-#include <stdlib.h>
+#include <math.h>
+using namespace std;
 
-#include "Foo.h"
+
+float dist3(float ux,float uy , float uz,float vx,float vy , float vz);
+
+
 int main()
 {
-  Foo d = Foo();
-  std::cout<< d.doSomething()<<std::endl;
 
-  return 0;
+  cout << dist3(1,2,3,7,-4,5) << endl;
+  cout << dist3(1,2,3,1,2,3) << endl;
+  cout << dist3(1,2,3,3,-8,5);
+
+
+}
+
+
+float dist3(float ux,float uy , float uz,float vx,float vy , float vz )
+{
+  float x,y,z,result1,result2,result3,result_main;
+  float list1[3] = {ux,uy,uz};
+  float list2[3] = {vx,vy,vz};
+
+  x = list1[0]-list2[0];
+  y = list1[1] - list2[1];
+  z = list1[2] - list2[2];
+  result1 = pow(x,2);
+  result2 = pow(y,2);
+  result3 = pow(z,2);
+  result_main = sqrt(result1+result2+result3);
+  return result_main;
+
 }
